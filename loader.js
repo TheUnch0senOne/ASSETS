64512968d826e9e6c202e1de80ed09c8da4b35b3
TheUnchosenOne <63002752+TheUnch0senOne@users.noreply.github.com>
2020-06-07 23:22:59 +0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,17 @@
-$("div[class='loader']");
+if (document.script.src.includes("jquery") !== true) {
+  console.log("Generating jquery");
+  let scr = document.createElement("SCRIPT");
+  scr.async = "true";
+  scr.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+  document.head.appendchild(scr);
+}
+{
+  let div = $("div[class='loader']")[0];
+  div.css({"--main-bg-color":"black","background-color":"black","color":"#accae8"});
+  let $img = $.create("IMG");
+  $img = $img.attr({
+    src = "https://cdn.jsdelivr.net/gh/TheUnch0senOne/ASSETS/"
+  });
+  $img = $img.css({ })
+  div.append($img);
+}
